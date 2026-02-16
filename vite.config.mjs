2e8bd8acd309c691f18cf5fce5d0d45cee65799a
sardarzainu <sardarzainu@gmail.com>
2026-02-16 16:59:32 +0500

    update docs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/website-demo/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
