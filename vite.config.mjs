9b006076883faca274adb48d54481a9450cd7bf0
sardarzainu <sardarzainu@gmail.com>
2026-03-07 19:29:24 +0500

    netlify

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/website-demo/",
+  base: "/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
